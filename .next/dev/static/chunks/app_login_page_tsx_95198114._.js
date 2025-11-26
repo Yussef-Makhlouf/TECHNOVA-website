3d67bf5030a6a6ef243fec9cd45b6912f3139079
Eslam <eslamhussin600@gmail.com>
2025-11-26 05:56:36 +0200

    make pull request from main

--- .next/dev/static/chunks/app_login_page_tsx_95198114._.js
+++ .next/dev/static/chunks/app_login_page_tsx_95198114._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/node_modules_38ea90a5._.js",
-  "static/chunks/_45eb1c85._.js"
+  "static/chunks/_7e65717e._.js"
 ],
     source: "dynamic"
 });
